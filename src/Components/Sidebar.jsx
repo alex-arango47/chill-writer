125f12dd95dd23f67519b4f952d3b58eb87b93a5
alex-arango47 <alexis.arango21@gmail.com>
2022-10-13 21:09:14 +1300

    changing save logic to work with local storage, preparing sidebar and dropmenu so it show the stored messages

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,26 +1,56 @@
 import React from 'react'
 import { useState } from 'react'
 import './sidebar.css'
-import DropMenu from './DropMenu'
 
 const Sidebar = () => {
 
   const [style, setStyle] = useState("sidebar-Btn")
-  const [menu, setMenu] = useState("")
+  const [messages, setMessages] = useState("")
+  const [menuState, setMenuState] = useState(0)
 
   const handleClick = () => {
-    style === "sidebar-Btn" ? setStyle("sidebar-Btn_Click") : setStyle("sidebar-Btn")
-    menu === "" ? setMenu(DropMenu) : setMenu("")
+    if(menuState === 0){
+      setMenuState(1)
+      setStyle("sidebar-Btn_Click")
+    } else {
+      setMenuState(0)
+      setStyle("sidebar-Btn")
+    }
+  }
+
+  const handleMenu = () => {
+    let dropMenu = document.querySelector("#messageMenu-Container")
+    let messageList = []
+    if(dropMenu.classList.contains("hide")){
+      dropMenu.classList.remove("hide")
+      dropMenu.classList.add("messageMenu-Container")
+    } else {
+      dropMenu.classList.remove("messageMenu-Container")
+      dropMenu.classList.add("hide")
+    }
+    if(localStorage.getItem("messages")){
+      messageList.push(JSON.parse(localStorage.getItem("messages")))
+      setMessages(messageList)
+    } else {
+      messages !== "There are no saved messages." ? setMessages("There are no saved messages.") : setMessages()
+    }
+  }
+
+  const handleSidebarChange = () => {
+    handleClick()
+    handleMenu()
   }
 
   return (
     <>
       <div className="sidebar-Container">
-        <button id="sidebar-Btn" className={style} onClick={handleClick}>
+        <button id="sidebar-Btn" className={style} onClick={handleSidebarChange}>
           <i className="fa-solid fa-chevron-left"></i>
         </button>
       </div>
-      {menu}
+      <div className='hide' id='messageMenu-Container'>
+        <h1>{messages}</h1>
+      </div>
     </>
   )
 }
--- src/Components/TextArea.jsx
+++ src/Components/TextArea.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {useState} from 'react'
 import './textArea.css'
 import './saveReset.css'
-// import Sidebar from './Sidebar' -- W.I.P --
+import Sidebar from './Sidebar' 
 
 const TextArea = () => {
 
@@ -16,35 +16,42 @@ const TextArea = () => {
         setMessage('')
     }
 
+    // Old saving method down below:
+    // const handleMessageSave = () => {
+    //     if (message === ""){
+    //         return alert("The message can't be empty.")
+    //     }
+    //     const fileData = JSON.stringify(message);
+    //     const blob = new Blob([fileData], { type: "text/plain" });
+    //     const url = URL.createObjectURL(blob);
+    //     const link = document.createElement("a");
+    //     let date = new Date()
+    //     date = date.toDateString()
+    //     link.download = `My note created on ${date} with Chill typer`;
+    //     link.href = url;
+    //     link.click();
+    // }
 
-    const handleMessageSave = () => {
+    let messages = []
+
+     const handleMessageSave = () => {
         if (message === ""){
             return alert("The message can't be empty.")
         }
-        const fileData = JSON.stringify(message);
-        const blob = new Blob([fileData], { type: "text/plain" });
-        const url = URL.createObjectURL(blob);
-        const link = document.createElement("a");
-        let date = new Date()
-        date = date.toDateString()
-        link.download = `My note created on ${date} with Chill typer`;
-        link.href = url;
-        link.click();
-    }
+        if (localStorage.getItem("messages")){
+            messages = JSON.parse(localStorage.getItem("messages"))
+        }   
 
+         messages.push(message)
+         messages = JSON.stringify(messages)
+         localStorage.setItem("messages", messages)
 
-        // ---------- W.I.P ----------
-    // const handleMessageSave = () => {
-    //     let messageStorage = message
-    //     if (messageStorage === ""){
-    //         return alert("The message can't be empty.")
-    //     }
-    //     let storageCount = Number(localStorage.getItem("counter"))
-    //     storageCount = storageCount + 1
-    //     localStorage.setItem("counter", Number(storageCount))
-    //     localStorage.setItem(storageCount, messageStorage)
-    // }
-    // -----------------------------------------------------------------------------
+        // Testing experiment down below:
+        //  let storageCount = Number(localStorage.getItem("counter"))
+        //  storageCount = storageCount + 1
+        //  localStorage.setItem("counter", Number(storageCount))
+        //  localStorage.setItem(storageCount, messageStorage)
+     }
 
     return (
         <>
@@ -53,7 +60,7 @@ const TextArea = () => {
                 <button className='saveReset-Btn' onClick={handleMessageReset}>Clear</button>
                 <button className='saveReset-Btn' onClick={handleMessageSave}>Save</button>
             </div>
-            {/* <Sidebar /> W.I.P */}
+            <Sidebar />
         </>
     )
 }
